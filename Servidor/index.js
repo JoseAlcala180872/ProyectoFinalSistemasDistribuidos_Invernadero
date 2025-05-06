2f0a869d201fde56ecf3cafedf0c8444dc94be50
JoseAlcala180872 <jose.alcala@potros.itson.edu.mx>
2025-05-05 20:22:53 -0700

    Implementación de sistema de cola de mensajes con RabbitMQ

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const db = require('./config/db');
 const DatoDAO = require('./dataAccess/DatoDAO');
 
@@ -21,6 +22,50 @@ async function main() {
     }).catch(error => {
         console.error('Error al desconectar a la base de datos:', error);
     });;
+=======
+const amqp = require('amqplib');
+const db = require('./config/db');
+const DatoDAO = require('./dataAccess/DatoDAO');
+
+const queue = 'datos_sensores';
+
+async function consumirCola() {
+    const conn = await amqp.connect('amqp://localhost');
+    const channel = await conn.createChannel();
+    await channel.assertQueue(queue, { durable: true });
+
+    channel.consume(queue, async (msg) => {
+        if (msg !== null) {
+            try {
+                const sensorData = JSON.parse(msg.content.toString());
+                console.log('Datos recibidos de RabbitMQ:', sensorData);
+
+                // Guardar los datos en la base de datos
+                await DatoDAO.crearDato(sensorData)
+                    .then(datoGuardado => {
+                        console.log('Dato guardado en la base de datos:', datoGuardado);
+                    })
+                    .catch(error => {
+                        console.error('Error al guardar el dato:', error);
+                    });
+            } catch (error) {
+                console.error('Error al procesar los datos recibidos:', error.message);
+            }
+            channel.ack(msg);
+        }
+    });
+}
+
+async function main() {
+    await db.conectar()
+        .then(() => {
+            console.log('Conexión establecida con éxito');
+            consumirCola();
+        })
+        .catch(error => {
+            console.error('Error al conectar a la base de datos:', error);
+        });
+>>>>>>> Stashed changes
 }
 
 main();
--- gateway/gateway.js
+++ gateway/gateway.js
@@ -1,14 +1,62 @@
 const WebSocket = require('ws');
+<<<<<<< Updated upstream
 
 const wss = new WebSocket.Server({ port: 3000 });
 
+=======
+const crypto = require('crypto');
+const amqp = require('amqplib');
+
+const { publicKey: clavePublica, privateKey: clavePrivada } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+});
+
+console.log('Clave pública:', clavePublica.export({ type: 'pkcs1', format: 'pem' }));
+console.log('Clave privada:', clavePrivada.export({ type: 'pkcs1', format: 'pem' }));
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+let channel;
+const queue = 'datos_sensores';
+
+// Conexión a RabbitMQ
+amqp.connect('amqp://localhost')
+    .then(conn => conn.createChannel())
+    .then(ch => {
+        channel = ch;
+        return channel.assertQueue(queue, { durable: true });
+    })
+    .catch(console.error);
+
+>>>>>>> Stashed changes
 wss.on('connection', (ws) => {
     console.log('Módulo conectado.');
 
     ws.on('message', (message) => {
         try {
+<<<<<<< Updated upstream
             const receivedData = JSON.parse(message);
             console.log('Datos recibidos:', JSON.stringify(receivedData));
+=======
+            const datosRecibidos = JSON.parse(message);
+
+            if (datosRecibidos.type === 'encryptedData') {
+                const datosDescifrados = crypto.privateDecrypt(
+                    {
+                        key: clavePrivada,
+                        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                    },
+                    Buffer.from(datosRecibidos.data, 'base64')
+                );
+                console.log('Datos recibidos:', datosDescifrados.toString());
+                if (channel) {
+                    channel.sendToQueue(queue, Buffer.from(datosDescifrados.toString()), { persistent: true });
+                    console.log("Datos enviados a la cola RabbitMQ correctamente");
+                } else {
+                    console.error('No se pudo enviar los datos: canal de RabbitMQ no disponible.');
+                }
+            }
+>>>>>>> Stashed changes
         } catch (error) {
             console.error('Error al procesar los datos:', error.message);
         }
